73876af1b9b0b4024e25a5c47928b4c94f05e3a1
AmauSup <amaurydemaillegmail.com>
2026-02-25 11:59:20 +0100

    Initial Commit

---
+++ NutriGuessTests/NutriGuessTests.swift
@@ -0,0 +1,16 @@
+//
+//  NutriGuessTests.swift
+//  NutriGuessTests
+//
+//  Created by Etudiants on 25/02/2026.
+//
+
+import Testing
+
+struct NutriGuessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
